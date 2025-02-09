46a8fb3762257f57b5f52d7721da7b50856628df
ChuffedDom <98715130+ChuffedDom@users.noreply.github.com>
2025-02-09 02:19:12 +0000

    Added API to confirm email

--- api/main.py
+++ api/main.py
@@ -1,10 +1,37 @@
 from flask import Flask, request
+import os
+from dotenv import load_dotenv
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 app = Flask(__name__)
+
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
 
 @app.route('/', methods=['GET'])
 def health_check():
     return {'message': 'API is working'}
+
+@app.route('/confirm-email', methods=['POST'])
+def confirm_email():
+    print(f"Starting subscription flow request: {request.json}")
+    # get the body of the request
+    body = request.json
+    message = Mail(
+        from_email="dom@itsyourturn.news",
+        to_emails=body["email"],
+        subject="It's Your Turn",
+        html_content=open(os.path.join(os.path.dirname(__file__), f"../templates/{body["template"]}")).read()
+    )
+    try:
+        sg = SendGridAPIClient(SENDGRID_API_KEY)
+        response = sg.send(message)
+        if response.status_code == 202:
+            print("Email sent")
+            return {'message': 'email sent'}  
+    except Exception as e:
+        print(e)
+        print("Email not sent")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- tests/api.py
+++ tests/api.py
@@ -2,7 +2,7 @@ import time
 import datetime
 import requests
 
-def test_up_request(url):
+def api_up_request(url):
     print("\n" + 100 * "=")
     print(f"📋 Testing GET request to {url}...")
     try:
@@ -27,11 +27,44 @@ def test_up_request(url):
     except requests.exceptions.RequestException as e:
         print(f"❌ Failed to retrieve data: {e}")
 
+def send_subscribe_email(url, email, template):
+    print("\n" + 100 * "=")
+    print(f"📋 Testing email subscribe request {url} for {email} with template {template}...")
+    try:
+        # Send a POST request with the email and template
+        response = requests.post(
+            url,
+            json={"email": email, "template": template}
+        )
+        # Check if the response status code is 200 (OK)
+        if response.status_code == 200:
+            # Parse the JSON response
+            data = response.json()
+            
+            # Extract the message field from the response
+            message = data.get('message', '')
+            
+            # Print the test result based on the message field
+            if message == "email sent":
+                print("✅ Test passed")
+            else:
+                print("❌ Test failed")
+        else:
+            # Print an error message if the request was not successful
+            print(f"❌ Failed to send email. Status code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"❌ Failed to send email: {e}")
 
 # The tests to run
 print("\n" + 100 * "=")
 print(f"🚦 Running tests at {datetime.datetime.now()} ...")
-test_up_request("http://localhost:5000/")
-test_up_request("http://automations.chuffed.app")
-test_up_request("https://automations.chuffed.app")
+api_up_request("http://localhost:5000/")
+time.sleep(1)
+api_up_request("http://automations.chuffed.app")
+time.sleep(1)
+api_up_request("https://automations.chuffed.app")
+time.sleep(1)
+send_subscribe_email("http://localhost:5000/confirm-email", "dom@chuffed.solutions", "its-your-turn-subscription.html")
+time.sleep(1)
+send_subscribe_email("https://automation.chuffed.app/confirm-email", "dom@chuffed.solutions", "its-your-turn-subscription.html")
 
